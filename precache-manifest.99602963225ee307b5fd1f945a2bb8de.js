90f4449aa96009fc3bdb5a7bc3362e89c8d95906
Thi Nguyen <tsokye346@gmail.com>
2020-06-11 17:50:36 -0700

    Updates

---
+++ precache-manifest.99602963225ee307b5fd1f945a2bb8de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0702ea8c25d5c7ba2dabacbee6c4ce",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "6645f9f4d0046b24dc35",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7a9f6a1a18340b556b99",
+    "url": "/RoboFriends/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "6645f9f4d0046b24dc35",
+    "url": "/RoboFriends/static/js/2.e32d22c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboFriends/static/js/2.e32d22c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9f6a1a18340b556b99",
+    "url": "/RoboFriends/static/js/main.8a5c9e5d.chunk.js"
+  },
+  {
+    "revision": "af258257253a0ffa7f4f",
+    "url": "/RoboFriends/static/js/runtime-main.288d7cdc.js"
+  }
+]);
